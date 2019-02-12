808c1e2c79ca869e40901c06d95113f392bf61a2
g1st <gintstan@gmail.com>
2019-02-12 19:37:03 +0000

    Already have a user, no need for more.

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,37 +7,38 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, secret);
 }
 
-exports.signin = function (req, res) {
+exports.signin = function(req, res) {
   res.send({ token: tokenForUser(req.user) });
 };
 
-exports.signup = function (req, res, next) {
-  const email = req.body.email;
-  const password = req.body.password;
+// uncoment to allow signing up
+// exports.signup = function (req, res, next) {
+//   const email = req.body.email;
+//   const password = req.body.password;
 
-  if (!email || !password) {
-    return res.status(422).send({ error: 'Email and password must be provided' });
-  }
+//   if (!email || !password) {
+//     return res.status(422).send({ error: 'Email and password must be provided' });
+//   }
 
-  User.findOne({ email: email }, function (err, existingUser) {
-    if (err) {
-      return next(err);
-    }
+//   User.findOne({ email: email }, function (err, existingUser) {
+//     if (err) {
+//       return next(err);
+//     }
 
-    if (existingUser) {
-      return res.status(422).send({ error: 'Email is aleready in use...' });
-    }
+//     if (existingUser) {
+//       return res.status(422).send({ error: 'Email is aleready in use...' });
+//     }
 
-    const user = new User({
-      email: email,
-      password: password
-    });
+//     const user = new User({
+//       email: email,
+//       password: password
+//     });
 
-    user.save(function (err) {
-      if (err) {
-        return next(err);
-      }
-      res.json({ token: tokenForUser(user) });
-    });
-  });
-};
+//     user.save(function (err) {
+//       if (err) {
+//         return next(err);
+//       }
+//       res.json({ token: tokenForUser(user) });
+//     });
+//   });
+// };
