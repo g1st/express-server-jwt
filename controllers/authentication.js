2fd19af691d8f5d1849adcdce14a516894d4beb7
g1st <gintstan@gmail.com>
2019-05-05 16:08:46 +0100

    expose signup

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,44 +1,46 @@
 const jwt = require('jwt-simple');
 const User = require('../models/User');
-const { secret } = require('../config');
+require('dotenv').config();
+
+const { SECRET } = process.env;
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
-  return jwt.encode({ sub: user.id, iat: timestamp }, secret);
+  return jwt.encode({ sub: user.id, iat: timestamp }, SECRET);
 }
 
 exports.signin = function(req, res) {
   res.send({ token: tokenForUser(req.user) });
 };
 
-// uncoment to allow signing up
-// exports.signup = function (req, res, next) {
-//   const email = req.body.email;
-//   const password = req.body.password;
+exports.signup = function(req, res, next) {
+  const { email, password } = req.body;
 
-//   if (!email || !password) {
-//     return res.status(422).send({ error: 'Email and password must be provided' });
-//   }
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'Email and password must be provided' });
+  }
 
-//   User.findOne({ email: email }, function (err, existingUser) {
-//     if (err) {
-//       return next(err);
-//     }
+  User.findOne({ email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
 
-//     if (existingUser) {
-//       return res.status(422).send({ error: 'Email is aleready in use...' });
-//     }
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is aleready in use...' });
+    }
 
-//     const user = new User({
-//       email: email,
-//       password: password
-//     });
+    const user = new User({
+      email,
+      password
+    });
 
-//     user.save(function (err) {
-//       if (err) {
-//         return next(err);
-//       }
-//       res.json({ token: tokenForUser(user) });
-//     });
-//   });
-// };
+    user.save(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.json({ token: tokenForUser(user) });
+    });
+  });
+};
--- router.js
+++ router.js
@@ -1,5 +1,5 @@
-const Authentication = require('./controllers/authentication');
 const passport = require('passport');
+const Authentication = require('./controllers/authentication');
 require('./services/passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
@@ -16,5 +16,5 @@ module.exports = function(app) {
 
   app.post('/signin', requireSignIn, Authentication.signin);
 
-  // app.post('/signup', Authentication.signup);
+  app.post('/signup', Authentication.signup);
 };
