3a9d57795ff79e473f6a3fa21ab0332f265c9b46
g1st <gintstan@gmail.com>
2019-05-07 11:50:10 +0100

    combating cors

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ const { PORT, MONGODB_URL } = process.env;
 const app = express();
 app.use(cors());
 app.options('*', cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
+
 mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true });
 
 app.use(morgan('combined'));
